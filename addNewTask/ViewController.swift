b889ac0a2485092e002816c629e3667a09f94414
Arnold Sidiprasetija <arnolds1809@gmail.com>
2022-04-05 15:43:41 +0700

    Initial Commit

---
+++ addNewTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  addNewTask
+//
+//  Created by Arnold Sidiprasetija on 05/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
